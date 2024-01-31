faffccb5fe3f8e528f9e36b54412ae31cb851fe6
Nghia <19204686+nghiamphan@users.noreply.github.com>
2024-01-30 22:34:51 -0400

    add more default movie lists

--- populate_database/createDefaultList.js
+++ populate_database/createDefaultList.js
@@ -165,9 +165,12 @@ const main = async () => {
         { file: 'AFI 100 Cheers (2006).txt', name: 'AFI 100 Cheers (2006)' },
         { file: 'AFI 25 Musicals (2006).txt', name: 'AFI 25 Musicals (2006)' },
         { file: 'AFI 10 Top 10 (2008).txt', name: 'AFI 10 Top 10 (2008)' },
+        { file: 'BFI Sight and Sound Top 250 (2022).txt', name: 'BFI Sight & Sound Top 250 (2022)' },
+        { file: 'BFI 100 British Films (1999).txt', name: 'BFI 100 British Films (1999)' },
+        { file: '100 Italian Films to be saved (2008).txt', name: '100 Italian Films to be saved (2008)' },
         { file: 'TSPDT Top 1000 (2024).txt', name: "They Shoot Pictures, Don't They? Top 1000 (2024)" },
         { file: 'TSPDT 1001-2500 (2024).txt', name: "They Shoot Pictures, Don't They? 1001-2500 (2024)" },
-        { file: 'TSPDT 2501-5000 (2024).txt', name: "They Shoot Pictures, Don't They? 2501-5000 (2024)" }, // Missing 4 films
+        { file: 'TSPDT 2501-5000 (2024).txt', name: "They Shoot Pictures, Don't They? 2501-5000 (2024)" }, // Missing 3 films
     ]
 
     list.map(async ({ file, name }, index) => {
